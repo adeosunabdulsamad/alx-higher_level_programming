fdd8d71ae3a5c788f641c743d894b049bf94ec83
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-02-06 03:16:52 -0800

    Updated 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,8 @@
 
 def inherits_from(obj, a_class):
     """This is the function for subclass instnace"""
-    return issubclass(obj, a_class)
+    b_class = type(obj)
+    if b_class == a_class:
+        return False
+    else:
+        return issubclass(b_class, a_class)
