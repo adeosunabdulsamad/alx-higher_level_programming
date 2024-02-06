b091d010299df70d1a7ea6d9504a1a85d292e018
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-02-06 01:08:05 -0800

    Fixed lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""This module provides a function for looking up all attributes and methods of an object."""
+
 def lookup(obj):
     """The lookup function returns all attributes and methods of an object"""
-
+    
     return dir(obj)
+
