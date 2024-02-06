5d68f2798ce29922c08c6a4d36bb1dfca938cb68
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-02-06 02:52:02 -0800

    updated 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This is the module for subclass"""
+
+
+def inherits_from(obj, a_class):
+    """This is the function for subclass instnace"""
+    return issubclass(obj, a_class)
