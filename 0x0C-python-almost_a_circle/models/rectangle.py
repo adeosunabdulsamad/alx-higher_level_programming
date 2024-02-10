f6981169bce3faf94622d085de0e14143373bdad
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-02-10 10:03:16 -0800

    Enforced the the integer data type

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,6 +10,12 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """This is the rectangle class constructor"""
+        params =[("width", width), ("height", height), ("x", x), ("y", y)]
+        for name, value in params:
+            if not isinstance(value, int):
+                raise TypeError(f"{name} must be an integer")
+            else:
+                continue
         super().__init__(id)
         self.__width = width
         self.__height = height
