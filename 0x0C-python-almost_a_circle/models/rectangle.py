9a9c1a709abc95a497bcb4041810681e81cc55e8
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-02-12 15:53:39 +0100

    Updated Update method to include kwargs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,14 +92,19 @@ class Rectangle(Base):
         x = self.__x
         return f"[Rectangle] ({ide}) {x}/{y} - {w}/{h}"
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """This method updates the Rectangle Class"""
-        self.id = args[0]
-        if len(args) > 1:
-            self.__width = args[1]
-        if len(args) > 2:
-            self.__height = args[2]
-        if len(args) > 3:
-            self.__x = args[3]
-        if len(args) > 4:
-            self.__y = args[4]
+        if args:
+            self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1 ]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
+
+        if kwargs:
+            for key, value in kwarg.items():
+                setattr(self, key, value)
